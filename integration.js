5d9e407703704b3ed83dd1c3d2df1f7156023342
abdelkader <bouzomitaabdelkader@gmail.com>
2023-06-06 17:53:58 +0100

    refactor: improve performance

--- integration.js
+++ integration.js
@@ -1,8 +1,7 @@
 console.log('success1');
-console.log('success2');
+
 let newElement = document.createElement('h1');
 newElement.innerHTML = 'Hello, world!';
-console.log('success3');
-console.log('success4');
-console.log('success6');
+
 document.body.appendChild(newElement);
+console.log(document.getElementById('.title'))
