2452d7c7bf68b974786f590fe9cce15ca829f364
abdelkader <bouzomitaabdelkader@gmail.com>
2023-06-06 16:50:22 +0100

    docs: update documentation

---
+++ integration.js
@@ -0,0 +1,11 @@
+console.log('success1');
+console.log('success2');
+let newElement = document.createElement('h1');
+newElement.innerHTML = 'Hello, world!';
+console.log('success3');
+console.log('success4');
+console.log('success6');
+document.body.appendChild(newElement);
+
+alert('hello world');
+alert('success');
