ea546263af65324ec24c32fe03805eb436bc553a
Abdelkader bouzomita <108610803+Abdelkaderbzz@users.noreply.github.com>
2023-06-09 09:09:50 +0100

    Update integration.js

--- integration.js
+++ integration.js
@@ -1,13 +1,19 @@
-import io from "socket.io-client";
-console.log('hello world');
-let newElement = document.createElement('h1');
-newElement.innerHTML = 'Hello, world!';
-let titleElement = document.querySelector('.title');
-titleElement.textContent = "hello world";
-const socket = io('http://localhost:3001');
 
-// Listen for the "sendHtmlPopup" event
-socket.on('sendHtmlPopup', (string) => {
-  console.log(string); // Log the received string
-  // Perform any other operations with the string value here
-}); 
+const App = () =>
+{
+  console.log("hello world")
+  const popupElement = document.createElement('div');
+  document.body.appendChild(popupElement);
+  const setPopup = (string) => {
+    popupElement.innerHTML = string;
+  };
+
+  const socket = io('http://localhost:3001');
+  socket.on('receiveMessage', (string) => {
+    console.log('Message from the server', string);
+    setPopup(string);
+  });
+};
+
+App();
+ 
