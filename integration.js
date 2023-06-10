c4e0a59b1bb53a0c46ce1d95b4582bb1e0475131
Abdelkader bouzomita <108610803+Abdelkaderbzz@users.noreply.github.com>
2023-06-10 18:13:06 +0100

    Update integration.js

--- integration.js
+++ integration.js
@@ -1,25 +1,31 @@
-
-const App = () =>
-{
-  console.log("hello world")
-const scriptTag = document.querySelector('script[token]');
-const token = scriptTag.getAttribute('token');
-console.log(token);
-const domain = window.location.hostname;
-console.log(domain);
+const App = () => {
+  console.log('hello world');
+  const scriptTag = document.querySelector('script[token]');
+  const token = scriptTag.getAttribute('token');
+  console.log(token);
+  const domain = window.location.hostname;
+  console.log(domain);
   const popupElement = document.createElement('div');
   document.body.appendChild(popupElement);
   const setPopup = (string) => {
     popupElement.innerHTML = string;
   };
   const socket = io('https://popupbackend.onrender.com');
-  socket.emit('sendTokenAndDomain',{domainName:domain,accessToken:token})
-  console.log("emited")
-  socket.on('receiveMessage', (string) => {
-    console.log('Message from the server', string);
-    setPopup(string);
+  socket.emit('sendTokenAndDomain', { domainName: "listninja.netlify.app", accessToken: token });
+  socket.on('responseAccess', (res) =>
+  {
+    console.log(res)
+    if (res?.success === true)
+    {
+      console.log("hello")
+      socket.on('receiveMessage', (string) => {
+        console.log('Message from the server', string);
+        setPopup(string);
+      });
+    }
   });
+  console.log('emited');
 };
 
 App();
- 
+
