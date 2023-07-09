9aca143b7d85a649b8f794b6d4cf83896f55f2af
Abdelkader bouzomita <108610803+Abdelkaderbzz@users.noreply.github.com>
2023-07-09 17:37:57 +0100

    Update integration.js

--- integration.js
+++ integration.js
@@ -1,31 +1,31 @@
 const App = () => {
-  console.log('hello world');
+
+  const domain = window.location.hostname;
+  console.log(domain)
   const scriptTag = document.querySelector('script[token]');
   const token = scriptTag.getAttribute('token');
-  console.log(token);
-  const domain = window.location.hostname;
-  console.log(domain);
+  console.log(token)
   const popupElement = document.createElement('div');
   document.body.appendChild(popupElement);
   const setPopup = (string) => {
     popupElement.innerHTML = string;
   };
-  const socket = io('https://popupbackend.onrender.com');
-  socket.emit('sendTokenAndDomain', { domainName: "listninja.netlify.app", accessToken: token });
-  socket.on('responseAccess', (res) =>
-  {
-    console.log(res)
-    if (res?.success === true)
-    {
-      console.log("hello")
+  const socket = io('http://localhost:4000');
+  // socket.emit('sendTokenAndDomain', {
+  //   domainName: domain,
+  //   accessToken: token,
+  // });
+  // socket.on('responseAccess', (res) => {
+  //   if (res?.success === true) {
       socket.on('receiveMessage', (string) => {
         console.log('Message from the server', string);
         setPopup(string);
       });
-    }
-  });
+    // }
+  // });
   console.log('emited');
 };
 
 App();
+
 
