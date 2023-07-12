95ca5be539e4fa3325e8667deeb9286d9c4c05de
Abdelkader bouzomita <108610803+Abdelkaderbzz@users.noreply.github.com>
2023-07-12 08:56:30 +0100

    Update integration.js

--- integration.js
+++ integration.js
@@ -8,34 +8,6 @@ document.body.appendChild(popupElement);
 const setPopup = (htmlBlock) =>
 {
   popupElement.innerHTML = htmlBlock;
-  // const button = document.getElementsByTagName('button');
-  // button[1].addEventListener("click", () =>
-  // {
-  //   const url = 'http://localhost:8000/api/auth/login';
-  //   const data = {
-  //     email: 'a@gmail.com',
-  //     password: 'eepqris123',
-  //   };
-  // fetch(url, {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify(data),
-  // })
-  //   .then((response) => {
-  //     if (!response.ok) {
-  //       throw new Error('Network response was not ok');
-  //     }
-  //     return response.json();
-  //   })
-  //   .then((result) => {
-  //     console.log(result);
-  //   })
-  //   .catch((error) => {
-  //     console.error('Error:', error);
-  //   });
-  // })
 };
 const socket = io('http://localhost:4000');
 // socket.emit('sendTokenAndDomain', {
@@ -47,7 +19,7 @@ const socket = io('http://localhost:4000');
 socket.on('receiveMessage', (payload) =>
 {
   const {domain,htmlData}=payload
-  console.log(htmlData);
+  console.log(domain===window.location.host);
   if (domain === window.location.host)
   {
     console.log(htmlData)
@@ -56,5 +28,4 @@ socket.on('receiveMessage', (payload) =>
 });
 // }
 // });
-
 
