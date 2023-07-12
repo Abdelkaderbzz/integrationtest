8b01f49bfaaee317e379cab8c17a5562932cc774
Abdelkader bouzomita <108610803+Abdelkaderbzz@users.noreply.github.com>
2023-07-12 08:37:29 +0100

    Update integration.js

--- integration.js
+++ integration.js
@@ -1,31 +1,60 @@
-const App = () => {
-
-  const domain = window.location.hostname;
-  console.log(domain)
-  const scriptTag = document.querySelector('script[token]');
-  const token = scriptTag.getAttribute('token');
-  console.log(token)
-  const popupElement = document.createElement('div');
-  document.body.appendChild(popupElement);
-  const setPopup = (string) => {
-    popupElement.innerHTML = string;
-  };
-  const socket = io('http://localhost:4000');
-  // socket.emit('sendTokenAndDomain', {
-  //   domainName: domain,
-  //   accessToken: token,
-  // });
-  // socket.on('responseAccess', (res) => {
-  //   if (res?.success === true) {
-      socket.on('receiveMessage', (string) => {
-        console.log('Message from the server', string);
-        setPopup(string);
-      });
-    // }
-  // });
-  console.log('emited');
+const domain = window.location.hostname;
+console.log(domain);
+const scriptTag = document.querySelector('script[token]');
+const token = scriptTag.getAttribute('token');
+console.log(token);
+const popupElement = document.createElement('div');
+document.body.appendChild(popupElement);
+const setPopup = (htmlBlock) =>
+{
+  popupElement.innerHTML = htmlBlock;
+  // const button = document.getElementsByTagName('button');
+  // button[1].addEventListener("click", () =>
+  // {
+  //   const url = 'http://localhost:8000/api/auth/login';
+  //   const data = {
+  //     email: 'a@gmail.com',
+  //     password: 'eepqris123',
+  //   };
+  // fetch(url, {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify(data),
+  // })
+  //   .then((response) => {
+  //     if (!response.ok) {
+  //       throw new Error('Network response was not ok');
+  //     }
+  //     return response.json();
+  //   })
+  //   .then((result) => {
+  //     console.log(result);
+  //   })
+  //   .catch((error) => {
+  //     console.error('Error:', error);
+  //   });
+  // })
 };
-
-App();
+const socket = io('http://localhost:4000');
+// socket.emit('sendTokenAndDomain', {
+//   domainName: domain,
+//   accessToken: token,
+// });
+// socket.on('responseAccess', (res) => {
+//   if (res?.success === true) {
+socket.on('receiveMessage', (payload) =>
+{
+  const {domain,htmlData}=payload
+  console.log(htmlData);
+  if (domain === window.location.host)
+  {
+    console.log(htmlData)
+    setPopup(htmlData);
+  }
+});
+// }
+// });
 
 
